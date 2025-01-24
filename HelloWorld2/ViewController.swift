4adb68dda74bd7d691781e052c5dca9937b3ca86
Морозов Евгений <evgenii.morozov@sparm.com>
2025-01-24 21:54:19 +0300

    Оптимизация

--- HelloWorld2/ViewController.swift
+++ HelloWorld2/ViewController.swift
@@ -16,15 +16,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let defaultCoreGraphics = CGRect(x: 100, y: 100, width: 100, height: 100)
+        let defaultCoreGraphics = CGRect(x: 0, y: 0, width: 100, height: 100)
         
         redLightLabel.bounds = defaultCoreGraphics
         yellowLightLabel.bounds = defaultCoreGraphics
         greenLightLabel.bounds = defaultCoreGraphics
         
-        redLightLabel.layer.cornerRadius = 50
-        yellowLightLabel.layer.cornerRadius = 50
-        greenLightLabel.layer.cornerRadius = 50
+        redLightLabel.layer.cornerRadius = defaultCoreGraphics.height / 2
+        yellowLightLabel.layer.cornerRadius = defaultCoreGraphics.height / 2
+        greenLightLabel.layer.cornerRadius = defaultCoreGraphics.height / 2
     }
 
     @IBAction func startButtonDidTapped() {
