9a879e719e20bae0bba37a6cdd0d1cfd7b0b858e
Морозов Евгений <evgenii.morozov@sparm.com>
2025-01-24 20:15:09 +0300

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by Evgeniy Morozov on 24.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
