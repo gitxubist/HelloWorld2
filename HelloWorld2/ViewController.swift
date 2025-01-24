0b3648d250a5523ab11e5af9eed1fbef56198160
Морозов Евгений <evgenii.morozov@sparm.com>
2025-01-24 21:49:16 +0300

    Добавлены макет светофора

--- HelloWorld2/ViewController.swift
+++ HelloWorld2/ViewController.swift
@@ -9,10 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightLabel: UIView!
+    @IBOutlet weak var yellowLightLabel: UIView!
+    @IBOutlet weak var greenLightLabel: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let defaultCoreGraphics = CGRect(x: 100, y: 100, width: 100, height: 100)
+        
+        redLightLabel.bounds = defaultCoreGraphics
+        yellowLightLabel.bounds = defaultCoreGraphics
+        greenLightLabel.bounds = defaultCoreGraphics
+        
+        redLightLabel.layer.cornerRadius = 50
+        yellowLightLabel.layer.cornerRadius = 50
+        greenLightLabel.layer.cornerRadius = 50
     }
 
-
+    @IBAction func startButtonDidTapped() {
+        yellowLightLabel.alpha = 1
+    }
 }
 
